162f3f5508b3b495d077b7a5c0504105980bb087
Donnie Santos <DonaldJSantos@Gmail.com>
2015-09-30 00:43:13 -0400

    Updates.

---
+++ Week 5 - Introduction to JQuery/SimpleJQueryExample2/scripts.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function ()
+{
+	$(".cell1").click(function ()
+	{
+		var currentClass = $(this).attr("class");
+
+		var nextClass;
+
+		if (currentClass == "cell1") nextClass = "cell2";
+		else if (currentClass == "cell2") nextClass = "cell3";
+		else nextClass = "cell1";
+
+		$(this).attr("class", nextClass);
+	});
+});
---
+++ Week 5 - Introduction to JQuery/SimpleJQueryExample3/scripts.js
@@ -0,0 +1,30 @@
+﻿var LastElement = {}
+var CurrentElement = {};
+
+function ToggleHighlight()
+{
+	var current = $(CurrentElement);
+	var last = $(LastElement);
+
+	if (last != null)
+	{
+		var lastText = last.text();
+		last.text(current.text());
+		if (lastText.length > 0) current.text(lastText);
+	}
+
+	current.removeClass("cell");
+	current.addClass("highlighted-cell");
+	last.removeClass("highlighted-cell");
+	last.addClass("cell");
+}
+
+$(document).ready(function ()
+{
+	$(".cell").click(function ()
+	{
+		CurrentElement = this;
+		ToggleHighlight();
+		LastElement = this;
+	});
+});
