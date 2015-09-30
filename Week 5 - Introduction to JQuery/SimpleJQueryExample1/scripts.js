999ca05403f3514d0537f6b774065e5d71045066
Donnie Santos <DonaldJSantos@Gmail.com>
2015-09-29 21:13:25 -0400

    Updates.

---
+++ Week 5 - Introduction to JQuery/SimpleJQueryExample1/scripts.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function ()
+{
+	$(".cell").click(function ()
+	{
+		if ($(this).attr("class") == "cell")
+		{
+			$(this).addClass("highlight");
+			$(this).removeClass("cell");
+		}
+		else
+		{
+			$(this).addClass("cell");
+			$(this).removeClass("highlight");
+		}
+	});
+});
--- Week 5 - Introduction to JQuery/Test/scripts.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function ()
-{
-	$(".cell").click(function ()
-	{
-		alert($(this));
-	});
-});
