ba47dc62ca268260297188d2df76de6cdfd4546f
Donnie Santos <DonaldJSantos@Gmail.com>
2015-12-06 00:31:47 -0500

    Updates.

--- Week 13 - Introduction to AngularJS/Assignment12/IntroductionToAngular/Scripts/Home.js
+++ Week 13 - Introduction to AngularJS/Assignment12/IntroductionToAngular/Scripts/Home.js
@@ -1,1 +1,23 @@
-﻿
+﻿function myController($scope) {
+}
+
+angular.element(document).ready(function ($scope) {
+
+    $.getJSON("http://localhost:52025/Home/GetBattingOrder", {}, function (data) {
+
+        data = JSON.parse(data);
+
+        $scope.battingOrder = [];
+
+        for (var i=0; i<data.BattingOrder.length; i++)
+        {
+            $scope.battingOrder.push({
+                "FirstName": data.BattingOrder[i].FirstName,
+                "LastName": data.BattingOrder[i].LastName
+            });
+        }
+
+        // This doesn't work. "$scope.$apply is not a function"
+        // $scope.$apply();
+    });
+});
